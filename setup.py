94178142726e976437285e0fe123b537764b31b2
Börni <boerni@gmail.com>
2013-02-03 19:35:02 +0100

    added tox.ini

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from setuptools import find_packages
 from distutils.core import setup
 
 
+try:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+except IOError:
+    README = ''
+
+
 version_tuple = __import__('django_js_reverse').VERSION
 version = '.'.join([str(v) for v in version_tuple])
 setup(
@@ -12,7 +18,7 @@ setup(
     version=version,
     license='MIT',
     description='Javascript url handling for Django that doesn\'t hurt.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    long_description=README,
     author='Bernhard Janetzki',
     author_email='boerni@gmail.com',
     url='https://github.com/version2/django-js-reverse',
