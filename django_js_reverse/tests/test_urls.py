1d22b9eb5aa701cfaedd6c7f169ed59d3cfa3137
Börni <boerni@gmail.com>
2013-08-24 15:52:56 +0200

    added test for unicode url names

--- django_js_reverse/tests/test_urls.py
+++ django_js_reverse/tests/test_urls.py
@@ -18,11 +18,14 @@ urlpatterns = patterns('',
                        url(r'^jsreverse/$', 'django_js_reverse.views.urls_js', name='js_reverse'),
 
                        # test urls
-                       url(r'^test_no_url_args/$', 'foo', name='test_no_url_args'),
-                       url(r'^test_one_url_args/(?P<arg_one>[-\w]+)/$', 'foo', name='test_one_url_args'),
+                       url(r'^test_no_url_args/$', 'foo',
+                           name='test_no_url_args'),
+                       url(r'^test_one_url_args/(?P<arg_one>[-\w]+)/$', 'foo',
+                           name='test_one_url_args'),
                        url(r'^test_two_url_args/(?P<arg_one>[-\w]+)-(?P<arg_two>[-\w]+)/$', 'foo',
                            name='test_two_url_args'),
-
+                       url(r'^test_unicode_url_name/$', 'foo',
+                           name=u'test_unicode_url_name'),
                        # test namespace
                        url(r'^ns/$', include(pattern_ns_2,  namespace='ns2'))
 )
--- django_js_reverse/tests/unit_tests.py
+++ django_js_reverse/tests/unit_tests.py
@@ -23,22 +23,30 @@ class JSReverseViewTestCase(TestCase):
         response = self.client.post('/jsreverse/')
         self.assertContains(response, "'test_no_url_args', ['test_no_url_args/', []]")
 
-    def _test_view_one_url_arg(self):
+    def test_view_one_url_arg(self):
         response = self.client.post('/jsreverse/')
         self.assertContains(response, "'test_one_url_args', ['test_one_url_args/%(arg_one)s/', ['arg_one']]")
 
-    def _test_view_two_url_args(self):
+    def test_view_two_url_args(self):
         response = self.client.post('/jsreverse/')
         self.assertContains(
             response, "test_two_url_args', ['test_two_url_args/%(arg_one)s\\u002D%(arg_two)s/', ['arg_one','arg_two']]")
 
+    def test_unicode_url_name(self):
+        response = self.client.post('/jsreverse/')
+        self.assertContains(response, "'test_unicode_url_name', ['test_unicode_url_name/', []]")
+
     @override_settings(JS_REVERSE_JS_VAR_NAME='Foo')
     def _test_js_var_name_changed_valid(self):
+        # This test overrides JS_REVERSE_JS_VAR_NAME permanent, so it's disabled by default.
+        # Needs to by tested as single test case
         response = self.client.post('/jsreverse/')
-        self.assertContains(response, 'exports.Foo = (function () {')
+        self.assertContains(response, 'this.Foo = (function () {')
 
     @override_settings(JS_REVERSE_JS_VAR_NAME='1test')
     def _test_js_var_name_changed_invalid(self):
+        # This test overrides JS_REVERSE_JS_VAR_NAME permanent, so it's disabled by default.
+        # Needs to by tested as single test case
         from django.core.exceptions import ImproperlyConfigured
         with self.assertRaises(ImproperlyConfigured):
             self.client.post('/jsreverse/')
