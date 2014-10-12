8e551d9f2b03b49a2d535be7c46cda8124399e46
krupin.dv <krupin.dv19@gmail.com>
2014-10-12 18:24:56 +0400

    setup fix

--- django_js_reverse/__init__.py
+++ django_js_reverse/__init__.py
@@ -4,14 +4,13 @@ VERSION = (0, 4, 1)
 import sys
 from os.path import join, dirname
 
-from django.core.files.storage import FileSystemStorage
-from django.core.files.base import ContentFile
-
-from . views import urls_js
-
-
 
 def save_js_file():
+    # placed here to past test and setup
+    from django.core.files.storage import FileSystemStorage
+    from django.core.files.base import ContentFile
+    from . views import urls_js
+
     package_path = dirname(__file__)
     location = join(package_path, 'static', 'django_js_reverse', 'js')
     file = 'reverse.js'
