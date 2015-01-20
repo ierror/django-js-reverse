005ca9de24da892acd188ea0aeebc45b6b7f9055
Paul Logston <code@logston.me>
2015-01-20 15:07:13 -0500

    Add templatetag to inline js_reverse javascript.

---
+++ django_js_reverse/templatetags/js_reverse.py
@@ -0,0 +1,18 @@
+from django import template
+
+from django_js_reverse.views import urls_js
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def js_reverse_inline(context):
+    """
+    Outputs a string of javascript that can generate URLs via the use
+    of the names given to those URLs.
+    """
+    if 'request' in context:
+        return urls_js(context['request']).content
+    else:
+        return u''
