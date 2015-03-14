fdd80b2a4d43fbd9994d1c26d0eec86732c1121d
michael-borisov <niro@tut.by>
2015-03-14 20:35:49 +0300

    Fix for tests

--- django_js_reverse/templatetags/js_reverse.py
+++ django_js_reverse/templatetags/js_reverse.py
@@ -6,8 +6,8 @@ from django_js_reverse.views import urls_js
 register = template.Library()
 
 
-@register.simple_tag()
-def js_reverse_inline():
+@register.simple_tag(takes_context=True)
+def js_reverse_inline(context):
     """
     Outputs a string of javascript that can generate URLs via the use
     of the names given to those URLs.
