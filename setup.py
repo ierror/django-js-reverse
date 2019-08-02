f6a3573fec4ea7e51741f59fe49ddc8fae7b4e77
Thomas Grainger <tagrain@gmail.com>
2019-08-02 11:51:17 +0100

    Back to development: 0.10.1a1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*parts):
 
 setup(
     name='django-js-reverse',
-    version='0.10.1a',
+    version='0.10.1a1.dev0',
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
