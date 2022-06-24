fea711ce5ba5faad9374e4a29e2f505d4e9a8a37
Patrik Beck <patrik.beck@gmail.com>
2022-06-24 11:24:02 +0200

    Add a django command: collectstatic_json_reverse which generates a JSON file for use in reactjs directly. Utilizing same code as collectstatic_js_reverse

--- django_js_reverse/management/commands/__init__.py
+++ django_js_reverse/management/commands/__init__.py
@@ -1,1 +1,21 @@
 __author__ = 'boerni'
+
+import os
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django_js_reverse.js_reverse_settings import JS_OUTPUT_PATH
+
+class CollectStaticCommand(BaseCommand):
+    requires_system_checks = False
+    def get_location(self):
+        output_path = getattr(settings, 'JS_REVERSE_OUTPUT_PATH', JS_OUTPUT_PATH)
+        if output_path:
+            return output_path
+
+        if not hasattr(settings, 'STATIC_ROOT') or not settings.STATIC_ROOT:
+            raise ImproperlyConfigured(
+                'The collectstatic_js_reverse command needs settings.JS_REVERSE_OUTPUT_PATH or settings.STATIC_ROOT to be set.')
+
+        return os.path.join(settings.STATIC_ROOT, 'django_js_reverse', 'js')
--- django_js_reverse/management/commands/collectstatic_json_reverse.py
+++ django_js_reverse/management/commands/collectstatic_json_reverse.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
+import json
 
 from django.conf import settings
 from django.core.files.base import ContentFile
 from django.core.files.storage import FileSystemStorage
-from django_js_reverse.core import generate_js
+from django_js_reverse.core import generate_json
 from django_js_reverse.management.commands import CollectStaticCommand
 
 try:
@@ -14,19 +15,19 @@ except ImportError:
 
 
 class Command(CollectStaticCommand):
-    help = 'Creates a static urls-js file for django-js-reverse'
+    help = 'Creates a static urls-json file for django-js-reverse'
     requires_system_checks = False
 
     def handle(self, *args, **options):
         location = self.get_location()
-        file = 'reverse.js'
+        file = 'reverse.json'
         fs = FileSystemStorage(location=location)
         if fs.exists(file):
             fs.delete(file)
 
         urlconf = getattr(settings, 'ROOT_URLCONF', None)
         default_urlresolver = get_resolver(urlconf)
-        content = generate_js(default_urlresolver)
+        content = json.dumps(generate_json(default_urlresolver))
         fs.save(file, ContentFile(content))
-        if len(sys.argv) > 1 and sys.argv[1] in ['collectstatic_js_reverse']:
+        if len(sys.argv) > 1 and sys.argv[1] in ['collectstatic_json_reverse']:
             self.stdout.write('%s file written to %s' % (file, location))  # pragma: no cover
