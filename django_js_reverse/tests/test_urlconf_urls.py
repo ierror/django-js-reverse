208a9fec3a26e9dbd79823708a68f8b28fbf1920
Daljit Singh <dsingh@brainomix.com>
2023-07-07 11:41:28 +0100

    Update to Dj 4.2

--- django_js_reverse/tests/test_urlconf_urls.py
+++ django_js_reverse/tests/test_urlconf_urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import url
+try:
+    from django.conf.urls import url
+except ImportError:
+    from django.urls import re_path as url
 from django.views.generic import View
 
 urlpatterns = [
